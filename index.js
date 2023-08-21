ffcc04f5b2de8943e936f2f4083e0aec52dccf6a
6upe <katongobupe444@gmail.com>
2023-08-20 20:56:19 -0700

    test deployment 1

--- index.js
+++ index.js
@@ -16,7 +16,38 @@ app.get('/', (req, res) => {
 });
 
 app.get('/data', (req, res) => {
-  res.json('data from the server');
+  const adverts = [
+      {
+          advertID: "1",
+          advertTitle: "Job Oppotunity: Software Developer",
+          advertBody: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+          advertCreatedAt: "22 Jan, 2023",
+          advertImagePath: "../procurement-images/procurement (1).jpg"
+      },
+      {
+          advertID: "2",
+          advertTitle: "Tender: Renovation of Ablution Block",
+          advertBody: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+          advertCreatedAt: "22 Jan, 2023",
+          advertImagePath: "../procurement-images/procurement (2).jpg"
+      },
+      {
+          advertID: "3",
+          advertTitle: "Supplier Registration",
+          advertBody: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+          advertCreatedAt: "22 Jan, 2023",
+          advertImagePath: "../procurement-images/procurement (3).jpg"
+      },
+      {
+          advertID: "4",
+          advertTitle: "Contract Awards",
+          advertBody: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+          advertCreatedAt: "22 Jan, 2023",
+          advertImagePath: "../procurement-images/procurement (4).jpg"
+      },
+  ];
+
+  res.json(adverts);
 });
 
 // Error handling middleware
