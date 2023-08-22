c448f5eea3c1804bb32afeff143e713a7b428437
6upe <katongobupe444@gmail.com>
2023-08-22 01:24:51 -0700

    mongodb connected

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
 const express = require('express');
 const cors = require('cors'); // Import the cors module
+require('dotenv').config(); // Load environment variables
+const mongoose = require('mongoose');
+const Admin = require('./database/models/admin');
 
+const port = process.env.PORT || 3000;
 const app = express();
 
 // Use the cors middleware
 app.use(cors());
 
+//database connection
+
+
+const mongoDBConnection = process.env.MONGODB_URI;
+
+mongoose.connect(mongoDBConnection, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+  console.log('Connected to MongoDB');
+})
+.catch((error) => {
+  console.error('Error connecting to MongoDB:', error);
+});
+
+
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    Admin.find()
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => {
+        console.log(err)
+      });
 });
 
 app.get('/data', (req, res) => {
   res.json('Hello, World!');
-});
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
 });
